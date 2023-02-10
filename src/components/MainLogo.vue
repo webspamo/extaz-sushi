ce8522f99bdcb83a6702a81fba6624b0e05e5d59
Yaroslav <webspamo@protonmail.com>
2023-02-10 20:01:41 +0200

    MainLogo

---
+++ src/components/MainLogo.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="logo-wrapper">
+        <a href="">
+            <img
+                src="@/assets/images/extazsushi_logo.svg"
+                alt="Extazsushi main logo"
+                class="logo-img" />
+        </a>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {};
+    },
+};
+</script>
+<style lang="scss">
+.logo-wrapper {
+}
+.logo-img {
+    height: auto;
+    width: 160px;
+    max-width: 160px;
+}
+</style>
