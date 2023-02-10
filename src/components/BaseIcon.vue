a741e861836e5e04784ad13c0ac072354cbb7ccf
Yaroslav <webspamo@protonmail.com>
2023-02-10 20:01:59 +0200

    BaseIcon

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,32 @@
+<template>
+    <svg-icon
+        type="mdi"
+        :path="iconComponent"></svg-icon>
+</template>
+<script>
+import SvgIcon from "@jamescoyle/vue-icon";
+import {mdiChevronDown} from "@mdi/js";
+
+export default {
+    name: "BaseIcon",
+    components: {
+        SvgIcon,
+    },
+    props: {
+        name: String,
+    },
+    data() {
+        return {
+            icons: {
+                "chevron-down": mdiChevronDown,
+            },
+        };
+    },
+    computed: {
+        iconComponent() {
+            return this.icons[this.name];
+        },
+    },
+};
+</script>
+<style lang="scss" scoped></style>
