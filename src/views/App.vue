1132ee9d616c89a32f8def56e3d4b1dc1d5679d2
Illia <dewy.thefirst@gmail.com>
2023-02-11 23:47:58 +0200

    pass props to MenuItem

--- src/views/App.vue
+++ src/views/App.vue
@@ -1,11 +1,17 @@
 <template>
     <div class="content">
-        <TheHeader></TheHeader>
+        <TheHeader />
 
         <div class="menu-items">
-            <MenuItem />
-            <MenuItem />
-            <MenuItem />
+            <MenuItem v-for="item in topMenuItems"
+                :key="item.id"
+                :name="item.name"
+                :description="item.description"
+                :weight="item.weight"
+                :weight-name="item.weightName"
+                :price="item.price"
+                :currency="item.currency"
+                :options="item.options" />
         </div>
 
         <TheFooter />
@@ -16,11 +22,33 @@ import TheHeader from "../components/TheHeader.vue";
 import TheFooter from "../components/TheFooter.vue";
 import MenuItem from "../components/MenuItem.vue";
 
+import { mapGetters } from "pinia";
+import { useSiteStore } from "@/stores/site";
+
 export default {
     name: "App",
     components: { TheHeader, MenuItem, TheFooter },
     data() {
-        return {};
+        return {
+            topMenuItemIds: ["euphoria", "euphoria", "euphoria", "euphoria", "euphoria"]
+        };
+    },
+    computed: {
+        ...mapGetters(useSiteStore, ["getMenuItemById"]),
+
+        topMenuItems() {
+            const items = [];
+
+            for (const itemId of this.topMenuItemIds) {
+                const item = this.getMenuItemById(itemId);
+
+                if (!item) continue;
+
+                items.push(item);
+            }
+
+            return items;
+        }
     },
 };
 </script>
@@ -30,6 +58,8 @@ export default {
     margin: 0 auto;
     gap: 1rem;
     max-width: 850px;
+    padding: 1rem;
+    flex-wrap: wrap;
 
     .menu-item {
         width: 100%;
