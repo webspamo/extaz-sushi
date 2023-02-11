5819383203a9a5debf827715d76c25fefaf8e27f
Illia <dewy.thefirst@gmail.com>
2023-02-11 23:02:14 +0200

    menuItems, getMenuItemById

--- src/stores/site.js
+++ src/stores/site.js
@@ -1,14 +1,37 @@
-import {defineStore} from "pinia";
+import { defineStore } from "pinia";
+
+const imagesPath = "@/assets/images/";
 
 export const useSiteStore = defineStore("site", {
     state: () => ({
         startHours: "12:00",
         endHours: "21:00",
-        items: [
+        menuItems: [
             {
-                name: "",
-                imgSrc: "",
+                id: "euphoria",
+                name: "Ейфорія рол",
+                image: imagesPath + "sushi-1.jpg",
+                description: "Обпалений, карамелізований унагі соусом лосось, креветка, ікра тобіко, авокадо.",
+                weight: 330,
+                price: 440,
+                options: {
+                    name: "Обрати",
+                    items: [
+                        {
+                            key: "losos",
+                            name: "Лосось",
+                            price: 10,
+                            weight: 10,
+                            image: ``
+                        }
+                    ]
+                }
             },
         ],
     }),
+    getters: {
+        getMenuItemById: (state) => {
+            return (itemId) => state.menuItems.find((item) => item.id === itemId)
+        },
+    },
 });
