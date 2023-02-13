2a0b2339f4091c98cbc15fad212d564c01db82f5
Yaroslav <webspamo@protonmail.com>
2023-02-13 18:58:50 +0200

    create InfoContent.vue component

---
+++ src/components/InfoContent.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="info-content container">
+        <div
+            class="info-card"
+            v-for="card in cards"
+            :key="card.id">
+            <img
+                :src="card.imagePath"
+                :alt="card.imageAlt" />
+            <h3 class="card-titile">
+                {{ card.title }}
+            </h3>
+            <p>
+                {{ card.text }}
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+import {mapState} from "pinia";
+import {useSiteStore} from "../stores/site";
+
+export default {
+    name: "InfoContent",
+    data() {
+        return {
+            cards: [
+                {
+                    id: "deliveryInfo",
+                    title: "Доставимо",
+                    text: "до дверей ~60хв безкоштовно по Печерському р-ну +10 грн за кожен наступний км",
+                    imagePath: "/src/assets/images/delivery-image.png",
+                    imageAlt: "Delivery outline image",
+                },
+                {
+                    id: "toGoInfo",
+                    title: "Самовиніс",
+                    text: "Печерський район, вул. Іоанна Павла || 7 Тільки за попереднім замовленням",
+                    imagePath: "/src/assets/images/to-go-image.svg",
+                    imageAlt: "Map point outline image",
+                },
+                {
+                    id: "paymentInfo",
+                    title: "Сплачуй зручно",
+                    text: "готівкою при отриманні карткою на сайті",
+                    imagePath: "/src/assets/images/payment-image.png",
+                    imageAlt: "Payment type outline image",
+                },
+                {
+                    id: "workingHoursInfo",
+                    title: "Працюємо",
+                    text: `щодня з ${this.startHours} до ${this.endHours}`,
+                    imagePath: "/src/assets/images/working-hours-image.svg",
+                    imageAlt: "Clock outline image",
+                },
+            ],
+        };
+    },
+
+    computed: {
+        ...mapState(useSiteStore, ["startHours", "endHours"]),
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+@use "@/assets/styles/_mixins";
+.info-content {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 2.5rem;
+}
+
+.info-card {
+    width: 255px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.card-titile {
+    text-transform: uppercase;
+    color: mixins.$main-accent;
+    font-size: 1.875rem;
+}
+</style>
