1b23c9fec7c30fc548fd919729208dc8ec1f887d
Yaroslav <webspamo@protonmail.com>
2023-02-13 21:38:19 +0200

    add "phone" field to siteStore

--- src/stores/site.js
+++ src/stores/site.js
@@ -4,6 +4,7 @@ export const useSiteStore = defineStore("site", {
     state: () => ({
         startHours: "12:00",
         endHours: "21:00",
+        phone: "0930551355",
     }),
     getters: {},
 });
