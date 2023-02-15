6af55ade34d9bb2a99b4e1aea52f9579b9b0fd3e
Yaroslav <webspamo@protonmail.com>
2023-02-15 20:08:58 +0200

    change quantity divider

--- src/components/BaseQuantity.vue
+++ src/components/BaseQuantity.vue
@@ -1,20 +1,27 @@
 <template>
     <div class="quantity">
-        <button type="button"
+        <button
+            type="button"
             :disabled="modelValue === 1"
             @click="$emit('less')"
             class="quantity-button">
             <BaseIcon name="minus-light"></BaseIcon>
         </button>
 
-        <input type="text"
+        <div class="quantity-divider"></div>
+
+        <input
+            type="text"
             required
             pattern="\d+"
             :value="modelValue"
             @input="$emit('update:modelValue', $event.target.value)"
             class="quantity-input" />
 
-        <button type="button"
+        <div class="quantity-divider"></div>
+
+        <button
+            type="button"
             @click="$emit('more')"
             class="quantity-button">
             <BaseIcon name="plus-light"></BaseIcon>
@@ -27,21 +34,22 @@ import BaseIcon from "./BaseIcon.vue";
 
 export default {
     name: "BaseQuantity",
-    components: { BaseIcon },
-    emits: ['less', 'more', 'update:modelValue'],
+    components: {BaseIcon},
+    emits: ["less", "more", "update:modelValue"],
     props: {
         modelValue: Number,
     },
     data() {
-        return {}
+        return {};
     },
-}
+};
 </script>
 
 <style lang="scss">
 @use "@/assets/styles/_mixins";
 
 .quantity {
+    position: relative;
     background-color: #fff;
 
     height: 45px;
@@ -57,45 +65,17 @@ export default {
 
         position: relative;
 
-        &:first-child {
-            padding: 0 0.3125rem;
-
-            &::after {
-                content: "";
-                position: absolute;
-
-                top: 50%;
-                right: 0;
-
-                transform: translateY(-50%);
-
-                display: block;
-                height: 100%;
-                width: 1px;
-                background-color: lightgray;
-            }
-        }
-
+        &:first-child,
         &:last-child {
             padding: 0 0.3125rem;
-
-            &::before {
-                content: "";
-                position: absolute;
-
-                top: 50%;
-                left: 0;
-
-                transform: translateY(-50%);
-
-                display: block;
-                height: 100%;
-                width: 1px;
-                background-color: lightgray;
-            }
         }
-
         cursor: pointer;
+    }
+
+    &-divider {
+        height: 65%;
+        width: 1px;
+        background-color: lightgray;
     }
 
     &-input {
