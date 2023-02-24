54aadb18ad805bdcd70f10f97ac664b81e7459f8
Yaroslav <webspamo@protonmail.com>
2023-02-24 23:49:15 +0200

    create DeliveryPaymentPage.vue

---
+++ src/views/DeliveryPaymentPage.vue
@@ -0,0 +1,33 @@
+<template>
+    <main>
+        <div class="section">
+            <div class="headings container">
+                <h1 class="main-title">Доставка та Оплата</h1>
+                <p class="main-subtitle">
+                    Замовляйте доставку свіжих та смачних ролів онлайн
+                </p>
+            </div>
+        </div>
+
+        <div class="section">
+            <div class="container">
+                <InfoContent />
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+import InfoContent from "../components/InfoContent.vue";
+
+export default {
+    name: "DeliveryPaymentPage",
+    components: {
+        InfoContent,
+    },
+    data() {
+        return {};
+    },
+};
+</script>
+<style lang="scss" scoped></style>
